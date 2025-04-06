a5d1836b94fc3a8336e35dc239dd1b1435e5fa1f
mitzie-ponce <mitzieponce0@gmail.com>
2025-04-06 22:28:54 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,9 @@
+let favActorFirstName = 'Your Actor First Name';
+let favActorLastName = 'Your Actor Last Name';
+let fullName = favActorFirstName + ' ' + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = 'My favorite actor is ${uppercase}. His best show is Silicon Valley.';
+
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
