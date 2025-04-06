d279ecba8d8f1d98353eac33fb6b1a3fa0449230
mitzie-ponce <mitzieponce0@gmail.com>
2025-04-06 22:14:20 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+let name = 'Your Name';
+let gender = 'Your Gender';
+let twitterHandle = '@YourTwitterHandle';
+
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(name);
+console.log(gender);
+console.log(twitterHandle);
