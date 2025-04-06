aba62a0d8cdddd8fa988503db0e576d7973c49a6
mitzie-ponce <mitzieponce0@gmail.com>
2025-04-06 22:20:51 +0800

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,10 @@
+let firstFavNum = 7;
+let secondFabNum = 10;
+
+console.log('Addition:', firstFavNum + secondFavNum);
+console.log('Subtraction:', firstFavNum - secondFavNum);
+console.log('Division:', firstFavNum / secondFavNum);
+console.log('Multiplication:', firstFavNum * secondFavNum);
+console.log('Modulus:', firstFavNum % secondFavNum);
+console.log('Exponentiation:', firstFavNum - secondFavNum);
+Math.pow(firstFavNum, 2));
